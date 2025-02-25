c16e764e08947b3994fb22500d1ed227318d2971
gp247 <gp247.net@gmail.com>
2025-02-25 21:53:28 +0700

    Loadd provider service

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,4 +2,7 @@
 
 return [
     App\Providers\AppServiceProvider::class,
+    
+    //Need to register all GP247 service providers here, load after GP247/core
+    GP247\Front\FrontServiceProvider::class
 ];
